cca5d009d9461d8d5d76f4b1d9d57abb7859ab58
SergeyParadigm77 <paradigm77@rambler.ru>
2023-09-06 23:29:45 +0300

    Создан бот, подключена БД

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -7,18 +7,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.service.TelegramSenderService;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
-
+    private final TelegramSenderService telegramSenderService;
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
-
-    @Autowired
-    private TelegramBot telegramBot;
-
+    private final TelegramBot telegramBot;
+    public TelegramBotUpdatesListener(TelegramSenderService telegramSenderService, TelegramBot telegramBot) {
+        this.telegramSenderService = telegramSenderService;
+        this.telegramBot = telegramBot;
+    }
     @PostConstruct
     public void init() {
         telegramBot.setUpdatesListener(this);
@@ -28,9 +30,23 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
-        });
-        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+            Long chatId = update.message().chat().id();
+            String message = update.message().text();
+
+            if ("/start".equals(message)) {
+                telegramSenderService.send(chatId, "Привет, друг!");
+            }
+            });
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        }
     }
 
-}
+
+
+
+
+
+
+
+
+
