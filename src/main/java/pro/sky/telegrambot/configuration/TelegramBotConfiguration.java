185fd1bb0f807a60e219bce71fb74a1f6aa363eb
SergeyParadigm77 <paradigm77@rambler.ru>
2023-09-13 23:13:51 +0300

    Добавлены модели, контроллеры, сервисы, репозитории

--- src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
+++ src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
@@ -2,15 +2,11 @@ package pro.sky.telegrambot.configuration;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.DeleteMyCommands;
-import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@Data
-@PropertySource("application.properties")
 public class TelegramBotConfiguration {
 
     @Value("${telegram.bot.token}")
